f78055edd78a883746b58a88ea81d9aacedb8c6f
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2016-07-12 14:15:49 +0200

    dcache-view: clean-up for release and deployment process
    Modification:
    
    1. deleted gulpfile_standalone.js
    2. adjusted pom.xml
    3. modified bower.json
    4. added some new task to the gulp file
    
    Result:
    
    No user visible changes.
    
    NOTE:
    
    To create a jar file for deployment:
        a. cd into the project root directory, and
        b. typed gulp
    The jar file created will be inside the target folder.
    
    Target: trunk
    Request: 1.0
    Require-notes: no
    Require-book: no
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    
    Reviewed at https://rb.dcache.org/r/9551/
    
    (cherry picked from commit 203ecb30cd3c196963b4aab0246c13a13f921654)

--- gulpfile.js
+++ gulpfile.js
@@ -67,30 +67,15 @@ gulp.task('vulcanize', function() {
         .pipe(gulp.dest('./target/elements'));
 });
 
-gulp.task('deploy-local', function(){
-    gulp.src('./target')
-        .pipe(zip('dcache-view.jar'))
-        .pipe(maven.install({
-            'groupId': 'org.dcache'
-        }))
+gulp.task('jar', function() {
+    return gulp.src('./target/*')
+        .pipe(zip('dcache-view-1.0.0.jar'))
+        .pipe(gulp.dest('./target'));
 });
 
-//Task for deploying to a Maven proxy
-/*gulp.task('deploy', function(){
-    gulp.src('.')
-        .pipe(zip('dcache-view.jar'))
-        .pipe(maven.deploy({
-            'groupId': 'org.dcache',
-            'repositories': [{
-                'id': 'some-repo-id',
-                'url': 'http://some-repo/url'
-            }]
-        }))
-});*/
 
 gulp.task('build', ['copy-index', 'copy-robots', 'copy-css', 'copy-script', 'copy-webcomponents', 'vulcanize']);
 
 gulp.task('default', ['bower', 'build'], function () {
-    gulp.start('deploy-local');
-    //gulp.start('deploy');
+    gulp.start('jar');
 });
