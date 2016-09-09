e40705a2d73a69371089afcb971f1244cdc1bc58
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2016-09-09 14:07:38 +0200

    dcache-view: add files upload functionality and feature
    Motivation:
    
    The upload feature is neccessary for any storage system like
    dcache. The new user interface of dcache, called dcache-view
    now implement the available facility inside dcache webdav for
    file/s upload.
    
    Modification:
    
    1. Creates an equivalent of a class in javascript to handle the
    upload.
    2. The upload-file-button element is added to the selected-title
    element as part of the tools that will be available for users. On
    click a file dialog-box will pop-up, which will allow the user to
    select file or multiple of files to be uploaded. When enter button
    on the keyboard or open button on the file dialog-box is hit, an
    object of upload class will be created for each file/s.
    3. An upload-toast-file element will display the status of the file.
    
    Result:
    
    User can upload file/s into a directory. The uploaded file/s will
    be added to the top of the list of files in that directory without
    reloading the page.
    
    Target: trunk
    Require-notes: no
    Require-book: no
    Acked-by: Paul Millar <paul.millar@desy.de>
    
    Reviewed at https://rb.dcache.org/r/9808/

---
+++ src/elements/dv-elements/utils/dcache-view-uploader/dcache-view-uploader.js
@@ -0,0 +1,88 @@
+'use strict';
+
+/**
+ * --- Adopted from ----
+ * https://github.com/googledrive/cors-upload-sample/blob/master/upload.js
+ *
+ * A class that can be use to upload any file into dcache.
+ *
+ * @constructor
+ * @param {object} options Hash of options
+ * @param {string} options.upauth Encoded Base64 of username:password
+ * @param {blob} options.file Blob-like item to upload
+ * @param {string} [options.contentType] Content-type, if overriding the type of the blob.
+ * @param {function} [options.onComplete] Callback for when upload is complete
+ * @param {function} [options.onProgress] Callback for status for the in-progress upload
+ * @param {function} [options.onError] Callback if upload fails
+ *
+ *  @example
+ *  var content = new Blob(["Hello world"], {"type": "text/plain"});
+ *  var uploader = new UploadHandler({
+ *      file: content,
+ *      upauth: xxxx,
+ *      onComplete: function(data) { ... },
+ *      onError: function(data) { ... }
+ *  })
+ *  uploader.upload();
+ */
+var UploadHandler = function (options)
+{
+    var noop = function() {};
+    this.file = options.file;
+    this.contentType = options.contentType || this.file.type || 'application/octet-stream';
+    this.upauth = options.upauth;
+    this.onComplete = options.onComplete || noop;
+    this.onProgress = options.onProgress || noop;
+    this.onError = options.onError || noop;
+    this.url = options.url;
+    this.httpMethod = 'PUT';
+};
+
+/**
+ * Upload the actual file content.
+ */
+UploadHandler.prototype.upload = function()
+{
+    var content = this.file;
+    var xhr = new XMLHttpRequest();
+    xhr.open(this.httpMethod, this.url, true);
+    xhr.setRequestHeader('Content-Type', this.contentType);
+    xhr.setRequestHeader('Authorization', 'Basic ' + this.upauth);
+    if (xhr.upload) {
+        xhr.upload.addEventListener('progress', this.onProgress);
+    }
+    xhr.onload = this.onContentUploadSuccess_.bind(this);
+    xhr.onerror = this.onContentUploadError_.bind(this);
+    xhr.send(content);
+};
+
+/**
+ * Handle successful responses for uploads. If complete, invokes
+ * the caller's callback.
+ *
+ * @private
+ * @param {object} e XHR event
+ */
+UploadHandler.prototype.onContentUploadSuccess_ = function(e)
+{
+    if (e.target.status == 200 || e.target.status == 201) {
+        this.onComplete(this.file);
+    } else {
+        this.onContentUploadError_(e);
+    }
+};
+
+/**
+ * Handles errors for uploads. [Client Side] error.
+ *
+ * @private
+ * @param {object} e XHR event
+ */
+UploadHandler.prototype.onContentUploadError_ = function(e)
+{
+    if (e.target.status && e.target.status < 500) {
+        this.onError(e.target);
+    } else {
+        this.onError(e.target);
+    }
+};
