a83e7cff6e2782ec28504cbf770864e4c3328aa5
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2016-09-27 13:33:00 +0200

    OK

--- gulpfile.js
+++ gulpfile.js
@@ -78,7 +78,7 @@ gulp.task('vulcanize', function() {
 
 gulp.task('jar', function() {
     return gulp.src('./target/**')
-        .pipe(zip('dcache-view-1.0.1.jar'))
+        .pipe(zip('dcache-view-1.0.2.jar'))
         .pipe(gulp.dest('./target'));
 });
 
