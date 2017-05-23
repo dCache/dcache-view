1d3f6e8fab8e6b7de93f3ecc67c70e9a6a6e0e8e
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2017-05-23 14:19:00 +0100

    dcache-view: upload files and directories using HTML5 drag&drop API
    Motivation:
    
    File(s) upload using HTML drag and drop API is a desirable
    feature in a web app like dcache-view. This will enable user
    to upload file by simply dragging the file (chrome & firefox
    support drag and drop for folder) into the current directory
    or a listed directory in the current view.
    
    Modification:
    
    1. Prevent the generic drag and drop on the page.
    
    2. Allow and listen to the drop, dragenter, dragleave, dragend,
    dragexit events in the namespace section of the page.
    
    3. Attached the neccessary drag & drop events to the list-row
    and pagination-button element. This will enable file(s) to be
    dropped on a listed dir. Also, if the user dragged and stayed
    on a listed directory for more than 2 seconds; this directory
    will be open and the user drag operation will still continue.
    
    4. Create two new elements namely `drag-enter-toast` & `dnd-upload`.
    The drag-enter-toast element notify the user when it enter
    into a drop zone. While dnd-upload element upload the files
    when dropped.
    
    5. Add a cental function that can be use to check the broswer type
    and version.
    
    Result:
    
    User can now drag and drop their files (and even folders if the
    browser support it) into dcache-view for uploading.
    
    **NB: drag and drop of folder is not supported in Safari yet.**
    
    Target: master
    Requires-notes: no
    Requires-book: no
    Acked-by: Tigran Mkrtchyan <tigran.mkrtchyan@desy.de>
    
    Reviewed at https://rb.dcache.org/r/10207/

--- src/scripts/dv.js
+++ src/scripts/dv.js
@@ -3,6 +3,11 @@
 
     var app = document.querySelector('#app');
 
+    // Global variables for monitoring drag and drop activities
+    let dndCounter = 0;
+    let timeoutID, dndArr = [];
+    app.closingTime = 3000;
+
     // Sets app default base URL
     app.baseUrl = '/';
 
@@ -119,6 +124,135 @@
         app.$.centralContextMenu.open();
     };
 
+    /**
+     * Get the file name from the file path
+     */
+    app.getfileName = function (path)
+    {
+        if (path === null || path === "" || path === "/") {
+            return 'Root';
+        } else {
+            let pt = path.endsWith('/') ? path.slice(0,-1): path;
+            return pt.slice(pt.lastIndexOf('/')).substring(1);
+        }
+    };
+
+    // Listing directory with time delay of @timeDelay
+    app.delayedLs = function (path, timeDelay)
+    {
+        timeoutID = window.setTimeout(()=>{
+            app.ls(path);
+            dndArr = [];
+            dndArr.length = 0;
+        },timeDelay);
+    };
+
+    // abort request to delayed listing directory
+    app.clearDelayedLs = function()
+    {
+        dndArr = [];
+        dndArr.length = 0;
+        window.clearTimeout(timeoutID);
+    };
+
+    app.delayTact = function (file)
+    {
+        dndArr.push(file);
+        const len = dndArr.length;
+        if (len === 1) {
+            app.delayedLs(file.__data__.filePath, 2000);
+        } else if (dndArr[len - 1].__data__.name !== dndArr[len - 2].__data__.name) {
+            app.clearDelayedLs();
+        }
+    };
+
+    /**
+     *
+     * current view drag and drop events listeners
+     */
+    app.drop = function(e)
+    {
+        let event = e || event;
+        event.preventDefault && event.preventDefault();
+
+        app.$.dropZoneToast.close();
+
+        let upload = new DndUpload(app.$.homedir.querySelector('view-file').path);
+        upload.start(event);
+
+        dndCounter = 0;
+    };
+    app.dragenter = function(e)
+    {
+        let event = e || event;
+        event.preventDefault && event.preventDefault();
+        dndCounter++;
+
+        app.$.dropZoneContent.querySelector('drag-enter-toast').directoryName =
+            app.getfileName(app.$.homedir.querySelector('view-file').path);
+        if (!app.$.dropZoneToast.opened){
+            app.$.dropZoneToast.open();
+        }
+    };
+    app.dragleave = function()
+    {
+        dndCounter--;
+    };
+    app.dragend = function()
+    {
+        dndCounter = 0;
+    };
+    app.dragexit = function()
+    {
+        dndCounter = 0;
+    };
+
+    app.checkBrowser = function ()
+    {
+        const ua = window.navigator.userAgent;
+        let tem = [];
+        let M = ua.match(/(opera|chrome|safari|firefox|msie|trident(?=\/))\/?\s*(\d+)/i);
+
+        if(/trident/i.test(M[1])) {
+            tem = /\brv[ :]+(\d+.?\d*)/g.exec(ua) || [];
+            return {name: 'Internet Explorer', version: tem[1]};
+        } else if(/firefox/i.test(M[1])) {
+            tem = /\brv[ :]+(\d+.?\d*)/g.exec(ua) || [];
+            return {name: 'Firefox', version: tem[1]};
+        } else if(/safari/i.test(M[1])) {
+            tem = ua.match(/\bVersion\/(\d+.?\d*\s*\w+)/);
+            return {name: 'Safari', version: tem[1]};
+        } else if(M[1] === 'Chrome') {
+            //opera
+            const temOpr = ua.match(/\b(OPR)\/(\d+.?\d*.?\d*.?\d*)/);
+            //edge
+            const temEdge = ua.match(/\b(Edge)\/(\d+.?\d*)/);
+            //chrome
+            const temChrome = ua.match(/\b(Chrome)\/(\d+.?\d*.?\d*.?\d*)/);
+            let genuineChrome = temOpr === null && temEdge === null && temChrome !== null;
+
+            if(temOpr !== null) {
+                return {name: temOpr[1].replace('OPR', 'Opera'), version: temOpr[2]};
+            }
+
+            if(temEdge !== null) {
+                return {name: temEdge[1], version: temEdge[2]};
+            }
+
+            if(genuineChrome) {
+                return {name: temChrome[1], version: temChrome[2]};
+            }
+        }
+    };
+
+    /**
+     * Reset the duration counter for the drag and drop Toast
+     */
+    app.dndToastClosed = function ()
+    {
+        app.closingTime = 3000;
+    };
+
     function updateFeListAndMetaDataDrawer(status, itemIndex)
     {
         if (app.$.metadata.selected == 'drawer'){
@@ -196,5 +330,23 @@
     window.addEventListener('iron-overlay-canceled', ()=> {
         app.$.homedir.querySelector('iron-list').selectionEnabled = false;
         setTimeout(()=>{app.$.homedir.querySelector('iron-list').selectionEnabled = true;},10)
+    });
+
+    // Prevent drag and drop default behaviour on the page
+    window.addEventListener('drag', function(event) {
+        event.preventDefault();
+        return false;
+    });
+    window.addEventListener('drop', function(event) {
+        event.preventDefault();
+        return false;
+    });
+    window.addEventListener('dragenter', function(event) {
+        event.preventDefault();
+        return false;
+    });
+    window.addEventListener('dragover', function(event) {
+        event.preventDefault();
+        return false;
     });
 })(document);
