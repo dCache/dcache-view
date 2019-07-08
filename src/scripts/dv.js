d63973f5e17c227b6994e24f3f9e5dd9c4c32fe4
Olufemi Adeyemi <olufemi.segun.adeyemi@desy.de>
2019-07-08 11:59:57 +0200

    dcache-view (webdav): re-request webdav door information
    Motivation:
    
    When a site does not allow anonymous operation, all information
    through the restful api including the ones that doesn't need to
    be protect will not be available, (e.g. webdav door api) if the
    user does not supply a known credentials.
    
    Modification:
    
    - include authentication header to the webdav door information
    request (if authentication credentials are available).
    - after successfully authentication, check if it is neccessary
    to make request for webdav door info.
    
    Result:
    
    Webdav door information will be requested again after a successful
    authentication provided this request failed initially during page
    load.
    
    Target: master
    Request: 1.5
    Require-notes: no
    Require-book: no
    
    Reviewed at https://rb.dcache.org/r/11863/

--- src/scripts/dv.js
+++ src/scripts/dv.js
@@ -678,6 +678,27 @@
         app.notifyPath('config.isSomebody');
         app.notifyPath('config.isAdmin');
         app.getQosInformation();
+        if (window.CONFIG['webdav'] === undefined) {
+            const webdavWorker = new Worker('scripts/tasks/request-webdav-endpoints.js');
+
+            webdavWorker.addEventListener('message', function(e) {
+                window.CONFIG["webdav"] = e.data;
+                webdavWorker.terminate();
+            }, false);
+
+            webdavWorker.addEventListener('error', function(e) {
+                console.info(e);
+                webdavWorker.terminate()
+            }, false);
+
+            webdavWorker.postMessage({
+                "apiEndpoint": `${window.CONFIG["dcache-view.endpoints.webapi"]}`,
+                "auth": app.getAuthValue(),
+                "protocol":
+                    `${window.location.protocol.endsWith(":") ?
+                        window.location.protocol.slice(0, -1): window.location.protocol}`
+            });
+        }
     });
     window.addEventListener('dv-namespace-open-upload-toast', (e) => {
         app.$.uploadToast.close();
